d1f9dc0738acdeaadc9365616a4ba7eb7913ea17
Lorena <lorena.tassi@outlook.com>
2020-04-21 08:29:26 +0200

    -

---
+++ src/com.koeftespiess/createRoom/CreateRoomController.java
@@ -0,0 +1,42 @@
+package com.koeftespiess.createRoom;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import jdk.jfr.Event;
+
+public class CreateRoomController {
+
+    @FXML
+    private Button bt;
+
+    @FXML
+    private Button btback;
+
+    @FXML
+    private Label namelb;
+
+    @FXML
+    private TextField nametxt;
+
+    @FXML
+    private Label seatlb;
+
+    @FXML
+    private TextField seattxt;
+
+    @FXML
+    private Label rowlb;
+
+    @FXML
+    private TextField rowtxt;
+
+    public void create(Event event) {
+
+    }
+
+    public void back(Event event) {
+
+    }
+}
